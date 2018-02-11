ede610b7ad154a902ee964f9e1701ac6de6e4b27
Ragnarok <DeathMetallDrummer@gmail.com>
2018-02-11 22:42:32 +0300

    Заменяет Jade на Pug.Заменяет пробельные отступы на таб-отступы.

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,35 @@
 // Blacklist    https://github.com/gulpjs/plugins/blob/master/src/blackList.json
 var gulp = require('gulp'),
-    jade = require('gulp-jade'),
-    sass = require('gulp-sass'),
-    plumber = require('gulp-plumber'),
-    rename = require("gulp-rename"),
-    browserSync = require('browser-sync'),
-    autoprefixer = require('gulp-autoprefixer'),
-    mini = require('gulp-clean-css'),
-    uglify = require('gulp-uglify'),
-    del = require('del'),
-    imagemin = require('gulp-imagemin'),
-    runSequence = require('run-sequence');
+	jade = require('gulp-jade'),
+	pug = require('gulp-pug'),
+	sass = require('gulp-sass'),
+	plumber = require('gulp-plumber'),
+	rename = require("gulp-rename"),
+	browserSync = require('browser-sync'),
+	autoprefixer = require('gulp-autoprefixer'),
+	mini = require('gulp-clean-css'),
+	uglify = require('gulp-uglify'),
+	del = require('del'),
+	imagemin = require('gulp-imagemin'),
+	runSequence = require('run-sequence');
 
 var pathDev = 'Source/Development/',
-       pathCom = 'Source/Completed/';
+	pathCom = 'Source/Completed/';
 
 var prefix = {
-          browsers: ['last 5 versions'],
-          cascade: false
-      }
+	browsers: ['last 5 versions'],
+	cascade: false
+}
 
 
 ///////////////////////////////////////////////////////////////////////////////////////
 //                                                              JADE
 ///////////////////////////////////////////////////////////////////////////////////////
-gulp.task('jade',function() {
-  return gulp.src(pathDev+'Jade/jade.jade')
-  .pipe(plumber())
-  .pipe(jade({pretty: true}))//pretty - древовидная структура
-  .pipe(rename({basename: 'index'}))
-  .pipe(gulp.dest(pathCom))
-});
-
-gulp.task('jade:pages',function() {
-  return gulp.src([
-      pathDev+'Jade/*.jade',
-      '!'+pathDev+'Jade/jade.jade'
-  ])
-  .pipe(plumber())
-  .pipe(jade({pretty: true}))//pretty - древовидная структура
-  .pipe(gulp.dest(pathCom))
-});
-
-gulp.task('Jade', function(){
-    runSequence('jade','jade:pages');
+gulp.task('Pug', function() {
+	return gulp.src(pathDev + 'Pug/*.{pug,jade}')
+		.pipe(plumber())
+		.pipe(pug()) //pretty - древовидная структура
+		.pipe(gulp.dest(pathCom))
 });
 
 
@@ -51,36 +37,40 @@ gulp.task('Jade', function(){
 //                                                              SASS
 ///////////////////////////////////////////////////////////////////////////////////////
 //sass - задача для главного файла стилей
-gulp.task('sass',function() {
-  return gulp.src(pathDev+'Sass/sass.sass')
-  .pipe(plumber())
-  .pipe(sass().on('error', sass.logError))
-  .pipe(autoprefixer(prefix))
-  .pipe(rename({basename: 'style'}))
-  .pipe(gulp.dest(pathCom+'Stylesheets/'))
+gulp.task('sass', function() {
+	return gulp.src(pathDev + 'Sass/sass.sass')
+		.pipe(plumber())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer(prefix))
+		.pipe(rename({
+			basename: 'style'
+		}))
+		.pipe(gulp.dest(pathCom + 'Stylesheets/'))
 });
 
 //sass:libs - задача для всех файлов стилей внутри папки Sass кроме главного
-gulp.task('sass:libs',function() {
-  return gulp.src([
-      pathDev+'Sass/*.{sass,scss}',
-      '!'+pathDev+'Sass/sass.sass'
-  ])
-  .pipe(plumber())
-  .pipe(sass().on('error', sass.logError))
-  .pipe(autoprefixer(prefix))
-  .pipe(gulp.dest(pathCom+'Stylesheets/'))
+gulp.task('sass:libs', function() {
+	return gulp.src([
+			pathDev + 'Sass/*.{sass,scss}',
+			'!' + pathDev + 'Sass/sass.sass'
+		])
+		.pipe(plumber())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer(prefix))
+		.pipe(gulp.dest(pathCom + 'Stylesheets/'))
 });
 
-gulp.task('css:min',function() {
-    return gulp.src([pathCom+'Stylesheets/*.css', '!'+pathCom+'Stylesheets/*.min.css'])
-    .pipe(mini())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest(pathCom+'Stylesheets/'))
+gulp.task('css:min', function() {
+	return gulp.src([pathCom + 'Stylesheets/*.css', '!' + pathCom + 'Stylesheets/*.min.css'])
+		.pipe(mini())
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest(pathCom + 'Stylesheets/'))
 });
 
-gulp.task('Sass', function(){
-    runSequence('sass','sass:libs','css:min');
+gulp.task('Sass', function() {
+	runSequence('sass', 'sass:libs', 'css:min');
 });
 
 
@@ -88,19 +78,21 @@ gulp.task('Sass', function(){
 //                                                              JAVASCRIPT
 ///////////////////////////////////////////////////////////////////////////////////////
 gulp.task('js', function() {
-  return gulp.src(pathDev+'JavaScript/javascript.js')
-  .pipe(gulp.dest(pathCom+'JavaScript/'))
+	return gulp.src(pathDev + 'JavaScript/javascript.js')
+		.pipe(gulp.dest(pathCom + 'JavaScript/'))
 });
 
-gulp.task('js:min',function() {
-  return gulp.src([pathCom+'JavaScript/*.js', '!'+pathCom+'JavaScript/*.min.js'])
-  .pipe(uglify())
-  .pipe(rename({suffix: '.min'}))
-  .pipe(gulp.dest(pathCom+'JavaScript/'))
+gulp.task('js:min', function() {
+	return gulp.src([pathCom + 'JavaScript/*.js', '!' + pathCom + 'JavaScript/*.min.js'])
+		.pipe(uglify())
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest(pathCom + 'JavaScript/'))
 });
 
-gulp.task('JavaScript', function(){
-    runSequence('js','js:min');
+gulp.task('JavaScript', function() {
+	runSequence('js', 'js:min');
 });
 
 
@@ -108,9 +100,9 @@ gulp.task('JavaScript', function(){
 //                                                              IMAGE MIN
 ///////////////////////////////////////////////////////////////////////////////////////
 gulp.task('imgMin', function() {
-  return gulp.src([pathDev+'Images/**/*.{png,jpg}', '!'+pathDev+'Images/sprite/**/*.*'])
-  .pipe(imagemin())
-  .pipe(gulp.dest(pathCom+'Images/'))
+	return gulp.src([pathDev + 'Images/**/*.{png,jpg}', '!' + pathDev + 'Images/sprite/**/*.*'])
+		.pipe(imagemin())
+		.pipe(gulp.dest(pathCom + 'Images/'))
 });
 
 
@@ -118,8 +110,8 @@ gulp.task('imgMin', function() {
 //                                                              COPY
 ///////////////////////////////////////////////////////////////////////////////////////
 gulp.task('copy', function() {
-  return gulp.src([pathDev+'{Images,Fonts}/**/*.*', '!'+pathDev+'Images/sprite/**/*.*'])
-  .pipe(gulp.dest(pathCom))
+	return gulp.src([pathDev + '{Images,Fonts}/**/*.*', '!' + pathDev + 'Images/sprite/**/*.*'])
+		.pipe(gulp.dest(pathCom))
 });
 
 
@@ -127,20 +119,20 @@ gulp.task('copy', function() {
 //                                                              CLEAN
 ///////////////////////////////////////////////////////////////////////////////////////
 gulp.task('clean', function() {
-    del(pathCom);
+	del(pathCom);
 });
 
 
 ///////////////////////////////////////////////////////////////////////////////////////
 //                                                              SERVER
 ///////////////////////////////////////////////////////////////////////////////////////
-gulp.task('server', function () {
-  browserSync({
-    port: 9000,
-    server: {
-      baseDir: pathCom
-    }
-  });
+gulp.task('server', function() {
+	browserSync({
+		port: 9000,
+		server: {
+			baseDir: pathCom
+		}
+	});
 });
 
 
@@ -148,10 +140,10 @@ gulp.task('server', function () {
 //                                                              WATCHING
 ///////////////////////////////////////////////////////////////////////////////////////
 gulp.task('watching', function() {
-    gulp.watch(pathCom+'**/*.{html,css,js}').on('change', browserSync.reload);
-    gulp.watch(pathDev+'**/*.{sass,scss}', ['Sass']);
-    gulp.watch(pathDev+'**/*.jade', ['Jade']);
-    gulp.watch(pathDev+'**/*.js', ['JavaScript']);
+	gulp.watch(pathCom + '**/*.{html,css,js}').on('change', browserSync.reload);
+	gulp.watch(pathDev + '**/*.{sass,scss}', ['Sass']);
+	gulp.watch(pathDev + '**/*.{pug,jade}', ['Pug']);
+	gulp.watch(pathDev + '**/*.js', ['JavaScript']);
 });
 
 // gulp.task('watching', function() {
@@ -167,7 +159,7 @@ gulp.task('watching', function() {
 //                                                              RUN
 ///////////////////////////////////////////////////////////////////////////////////////
 // dev
-gulp.task('dev',['Jade','Sass','JavaScript','copy']);
+gulp.task('dev', ['Pug', 'Sass', 'JavaScript', 'copy']);
 
 // default
-gulp.task('default', ['dev','server','watching']);
+gulp.task('default', ['dev', 'server', 'watching']);
